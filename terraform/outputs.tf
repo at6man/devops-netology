f0e626d76baf1900b9595698b51a599f28b6b907
Alexander Puzanok <at6man@ya.ru>
2021-10-17 12:13:39 +0300

    Homework 7.2

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "aws_account_id" {
+    value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_user_id" {
+    value = data.aws_caller_identity.current.user_id
+}
+
+output "aws_region" {
+    value = data.aws_region.current.name
+}
+
+output "aws_instance_ip_addr" {
+    value = aws_instance.server.private_ip
+}
+
+output "aws_instance_subnet_id" {
+    value = aws_instance.server.subnet_id
+}
